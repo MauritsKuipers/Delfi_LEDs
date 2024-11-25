0070610d04dbbc8e9d189141d825d618ffc0f889
Maurits_Kuipers <maurits.mdmlk@gmail.com>
2024-11-25 12:05:40 +0100

    Made the Classes and Link Budget Usingg the Naval PostGraduate Paper appears to be working (with the before seen very small geometric loss)

--- Link_Budget/LED_Budget.py
+++ Link_Budget/LED_Budget.py
@@ -2,118 +2,118 @@
 import numpy as np
 import math
 import matplotlib
-
-
-## Choices ##
-power_choice = "radiant_power"
-# power_choice = "radiant_intensity"
-
-
-
-
-## Data from the LED ##
-wavelength          = 660                           # [nm] red colour
-Uf                  = {350: 2.5, 1000: 4.1}         # Forward Voltage mA & V
-radiant_intensity   = {"min": 450, "typ": 850}      # [mW/sr] radiant intensity per squared radian (steradian aka the solid angle)
-radiant_power       = 260                           # [mW]
-LED_emiter_diameter = 5                             # [mm]
-divergence_angle    = np.deg2rad(180)               # [rad] This is a "guess" taken by looking at the LED and it looks like it is intended for 180deg dispersion
-forward_voltage     = 2.5                           # [V]
-current_drain       = 0.350                         # [A]
-LED_area            = 20 * 20                       # [mm2]
-
-## Orbit Properties ##
-h = 750 # [km]
-
-## Data from the Camara ##
-lens_diamter = 17*10**(-3) # [m]
-
-
-
-### Calculation ###
-## Some Physics ##
-
-if power_choice == "radiant_intensity":
-    # Solid Angles #
-    A1          = np.pi * (lens_diamter/2)**(2)  # [m2]
-    r           = h * 10**(3)                    # [m]
-    solidangle1 = A1 / r**(2)                    # [rad]
-
-    # Power Reached to the Camera #
-    P_transmitted_reached_area = radiant_intensity["typ"] * solidangle1     # Would this be geometric loss?
-    P_transmitted = 10 * np.log10(P_transmitted_reached_area / 1)           # [dBm]
-
-if power_choice == "radiant_power":
-    P_transmitted = 10 * np.log10( (radiant_power) / 1)       # [dBm]
-
-## Post-Doc Thesis ###
-# Extinction Loss Le #
-lambda1     = wavelength    # Wavelength of the emitted light
-delta1      = 1.6           # Particle Size Distribution Coefficient
-V1          = 100*10^3      # [m] Meteorological Visibility (as this is relatred to the atmosphere it is also capped at 100km)
-wavelength  = wavelength    # This is the wavelength of the emmited beam [nanometer]
-d1          = 100*10^3      # [m] Atmospheric Path Distance (This should be limited to 100km as maximum atmospheric thickness)
-
-alphae  = 3.91 / V1 * (lambda1 / 550)**(-delta1)
-
-Le      = -10 * np.log10(np.e**(-alphae * d1))      # dB
-
-# Geometric Loss Lgeom #
-Dr      = lens_diamter                              # The diameter of the receiving aperture [m]
-Dt      = LED_emiter_diameter * 10**(-3)            # [m] The diameter of transmitting aperture (might not be applicable to LED) [m]
-theta1  = divergence_angle * 10**(3)                # [mrad] Divergence Angle of the Beam [milli-radians]
-L1      = h                                         # Link Distance [km]
-
-Lgeom   = -20 * np.log10( Dr / (Dt + theta1 * L1))      # dB
-
-# Atmospheric Loss La #
-
-# (Need to revide this one)
 #
-# la      = 10 * np.log( np.sqrt(1 - sigmai ^ (2)) )
-
-
-# Total Link Budget #
-
-if power_choice == "radiant_intensity":
-    La = 0
-    Gr = 0
-    link_budget = P_transmitted - (Gr + La + Le)
-
-if power_choice == "radiant_power":
-    La = 0
-    Gr = 0                                                      # Assumed a receiver gain of 0 for safety
-    link_budget = P_transmitted - ( Gr + La + Le + Lgeom)
-
-if link_budget == 3 or link_budget > 3:
-    print("Link Budget Sufficient for the Margin Requirement of 3dB", link_budget)
-    print("Transmitted Power: ", P_transmitted)
-    print("Geometric Loss: ", Lgeom)
-    print("Extinction Losses: ", Le)
-else:
-    print("Link Budget Insufficient for the Margin Requirement of 3dB", link_budget)
-    print("Transmitted Power: ", P_transmitted)
-    print("Geometric Loss: ", Lgeom)
-    print("Extinction Losses: ", Le)
-
-print("========================================================================")
-
-### Estimating How Many LEDs Needed for Positive Link Budget of at Least 3dB ###
-# Estimation of Required Transmitted Power #
-Margin      = 3                                 # [dB] as assumed in meeting
-P_required  = 10 ** ((Lgeom + Le + Margin) / 10)
-
-# Estimation of Necessary Minimum Amount of LEDs #
-number_LEDs = math.ceil(P_required / radiant_power)
-print("Estimated Minimum Number of LEDs Needed: ", number_LEDs)
-
-# Some Requirements for the Satellite #
-
-power_drain = (forward_voltage * current_drain) * number_LEDs
-total_area  = LED_area * number_LEDs
-
-print("Required Power to Light the LEDs: ", power_drain, "W")
-print("Surface Area Needed to Place the LEDs: ", total_area, "mm2")
+#
+# ## Choices ##
+# power_choice = "radiant_power"
+# # power_choice = "radiant_intensity"
+#
+#
+#
+#
+# ## Data from the LED ##
+# wavelength          = 660                           # [nm] red colour
+# Uf                  = {350: 2.5, 1000: 4.1}         # Forward Voltage mA & V
+# radiant_intensity   = {"min": 450, "typ": 850}      # [mW/sr] radiant intensity per squared radian (steradian aka the solid angle)
+# radiant_power       = 260                           # [mW]
+# LED_emiter_diameter = 5                             # [mm]
+# divergence_angle    = np.deg2rad(180)               # [rad] This is a "guess" taken by looking at the LED and it looks like it is intended for 180deg dispersion
+# forward_voltage     = 2.5                           # [V]
+# current_drain       = 0.350                         # [A]
+# LED_area            = 20 * 20                       # [mm2]
+#
+# ## Orbit Properties ##
+# h = 750 # [km]
+#
+# ## Data from the Camara ##
+# lens_diamter = 17*10**(-3) # [m]
+#
+#
+#
+# ### Calculation ###
+# ## Some Physics ##
+#
+# if power_choice == "radiant_intensity":
+#     # Solid Angles #
+#     A1          = np.pi * (lens_diamter/2)**(2)  # [m2]
+#     r           = h * 10**(3)                    # [m]
+#     solidangle1 = A1 / r**(2)                    # [rad]
+#
+#     # Power Reached to the Camera #
+#     P_transmitted_reached_area = radiant_intensity["typ"] * solidangle1     # Would this be geometric loss?
+#     P_transmitted = 10 * np.log10(P_transmitted_reached_area / 1)           # [dBm]
+#
+# if power_choice == "radiant_power":
+#     P_transmitted = 10 * np.log10( (radiant_power) / 1)       # [dBm]
+#
+# ## Post-Doc Thesis ###
+# # Extinction Loss Le #
+# lambda1     = wavelength    # Wavelength of the emitted light
+# delta1      = 1.6           # Particle Size Distribution Coefficient
+# V1          = 100*10^3      # [m] Meteorological Visibility (as this is relatred to the atmosphere it is also capped at 100km)
+# wavelength  = wavelength    # This is the wavelength of the emmited beam [nanometer]
+# d1          = 100*10^3      # [m] Atmospheric Path Distance (This should be limited to 100km as maximum atmospheric thickness)
+#
+# alphae  = 3.91 / V1 * (lambda1 / 550)**(-delta1)
+#
+# Le      = -10 * np.log10(np.e**(-alphae * d1))      # dB
+#
+# # Geometric Loss Lgeom #
+# Dr      = lens_diamter                              # The diameter of the receiving aperture [m]
+# Dt      = LED_emiter_diameter * 10**(-3)            # [m] The diameter of transmitting aperture (might not be applicable to LED) [m]
+# theta1  = divergence_angle * 10**(3)                # [mrad] Divergence Angle of the Beam [milli-radians]
+# L1      = h                                         # Link Distance [km]
+#
+# Lgeom   = -20 * np.log10( Dr / (Dt + theta1 * L1))      # dB
+#
+# # Atmospheric Loss La #
+#
+# # (Need to revide this one)
+# #
+# # la      = 10 * np.log( np.sqrt(1 - sigmai ^ (2)) )
+#
+#
+# # Total Link Budget #
+#
+# if power_choice == "radiant_intensity":
+#     La = 0
+#     Gr = 0
+#     link_budget = P_transmitted - (Gr + La + Le)
+#
+# if power_choice == "radiant_power":
+#     La = 0
+#     Gr = 0                                                      # Assumed a receiver gain of 0 for safety
+#     link_budget = P_transmitted - ( Gr + La + Le + Lgeom)
+#
+# if link_budget == 3 or link_budget > 3:
+#     print("Link Budget Sufficient for the Margin Requirement of 3dB", link_budget)
+#     print("Transmitted Power: ", P_transmitted)
+#     print("Geometric Loss: ", Lgeom)
+#     print("Extinction Losses: ", Le)
+# else:
+#     print("Link Budget Insufficient for the Margin Requirement of 3dB", link_budget)
+#     print("Transmitted Power: ", P_transmitted)
+#     print("Geometric Loss: ", Lgeom)
+#     print("Extinction Losses: ", Le)
+#
+# print("========================================================================")
+#
+# ### Estimating How Many LEDs Needed for Positive Link Budget of at Least 3dB ###
+# # Estimation of Required Transmitted Power #
+# Margin      = 3                                 # [dB] as assumed in meeting
+# P_required  = 10 ** ((Lgeom + Le + Margin) / 10)
+#
+# # Estimation of Necessary Minimum Amount of LEDs #
+# number_LEDs = math.ceil(P_required / radiant_power)
+# print("Estimated Minimum Number of LEDs Needed: ", number_LEDs)
+#
+# # Some Requirements for the Satellite #
+#
+# power_drain = (forward_voltage * current_drain) * number_LEDs
+# total_area  = LED_area * number_LEDs
+#
+# print("Required Power to Light the LEDs: ", power_drain, "W")
+# print("Surface Area Needed to Place the LEDs: ", total_area, "mm2")
 
 
 
@@ -127,40 +127,108 @@ class LED:
     # Source: https://www.epigap-osa.com/Datasheets/Starboard/OCI-490-20_MUR_Star.pdf
     # COMMENT: This might become difficult to make an automatic parser as LEDs will have different Documentation (PROBLEM FOR FUTURE MAURITS)
     def __init__(self):
-        self.wavelength             = 660                           # [nm] red colour
-        self.Uf                     = {350: 2.5, 1000: 4.1}         # Forward Voltage mA & V
-        self.radiant_intensity      = {"min": 450, "typ": 850}      # mW/sr radiant intensity per squared radian (steradian aka the solid angle)
-        self.radiant_power          = 260                           # [mW]
-        self.LED_emiter_diameter    = 5                             # [mm]
-        self.divergence_angle       = np.deg2rad(180)               # [rad] This angle was determined "by visual inspection" as there is no data given but the LED looks like for 180 dispersion
-        self.performance            = {}                            # Possible Location to Save all Performance Characteristics Determined from the LinkBudget Calculations
+        self.wavelength                                 = 660                           # [nm] red colour
+        self.Uf                                         = {350: 2.5, 1000: 4.1}         # Forward Voltage mA & V
+        self.radiant_intensity                          = {"min": 450, "typ": 850}      # mW/sr radiant intensity per squared radian (steradian aka the solid angle)
+        self.radiant_power                              = 260                           # [mW]
+        self.LED_emitter_diameter                       = 5                             # [mm]
+        self.divergence_angle                           = np.deg2rad(180)               # [rad] This angle was determined "by visual inspection" as there is no data given but the LED looks like for 180 dispersion
+        self.performance_radiant_intensity              = {}                            # Possible Location to Save all Performance Characteristics Determined from the LinkBudget Calculations
+        self.performance_radiant_power                  = {}                            # Possible Location to Save all Performance Characteristics Determined from the LinkBudget Calculations
 
     # Maybe more function inside this class can be made in case some characteristics needed to determined from the given data #
 
-class Orbit:                                                        # Source: Dr. Speretta, Dr. Langbroek, Eventual Ir. Kuipers
+    def transmitted_power_link(self):
+        self.P_transmitted = 10 * np.log10((self.radiant_power) / 1)                    # [dBm] This is the transmitted power in for use in the link budget
+class Constants:
+    def __init__(self):
+        self.PlanetRadius = 6378137                                 # [km]
 
+class Orbit:                                                        # Source: Dr. Speretta, Dr. Langbroek, Eventual Ir. Kuipers
     def __init__(self):
         self.OrbitAltitude          = 750                           # [km] Assumed Maximum Orbit Altitude
         self.Elevation              = 40                            # [deg] REASONING TO BE GIVEN, TEMPORARY VALUE
 
 class DelftTelescope:                                               # Source:
     def __init__(self):
-        self.LensDiameter            = 17                           # [mm]
+        self.LensDiameter           = 17                            # [mm]
+        self.TelescopeHeight        = 0                             # [m] Assumed 0 for simplification and "worse case scenario"
+        self.maximum_distance       = None                          # Initiate the Attribute
+
+    def maximum_link_distance(self, orbit, constants):                         # This is calculated using: "Link budget calculation in optical LEO satellite downlinks with on/off-keying and large signal divergence: A simplified methodology"
+        L = np.sqrt( (constants.PlanetRadius + self.TelescopeHeight)**2 * (np.sin(np.deg2rad(orbit.Elevation)))**2 + 2*(orbit.OrbitAltitude - self.TelescopeHeight) *
+                     (constants.PlanetRadius + self.TelescopeHeight) + (orbit.OrbitAltitude - self.TelescopeHeight)**2 ) - (constants.PlanetRadius + self.TelescopeHeight) * np.sin(np.deg2rad(orbit.Elevation))
+        self.maximum_distance = L
 
 class LeidenTelescope:                                              # Source:
     def __init__(self):
         self.LensDiameter           = 00                            # [mm] TBD
+        self.TelescopeHeight        = 00                            # [m] Assumed 0 for simplification and "worse case scenario"
+        self.maximum_distance       = None                          # Initiate the Attribute
+
+    def maximum_link_distance(self, orbit):                         # This is calculated using: "Link budget calculation in optical LEO satellite downlinks with on/off-keying and large signal divergence: A simplified methodology"
+        L = np.sqrt( (orbit.PlanetRadius + self.TelescopeHeight)**2 * (np.sin(np.deg2rad(orbit.Elevation)))**2 + 2*(orbit.OrbitRadius - self.TelescopeHeight) *
+                     (orbit.PlanetRadius + self.TelescopeHeight) + (orbit.OrbitRadius - self.TelescopeHeight)**2 ) - (orbit.PlanetRadius + self.TelescopeHeight) * np.sin(np.deg2rad(orbit.Elevation))
+        self.maximum_distance = L
 
 class LinkBudget_Naval:                                             # Source: https://apps.dtic.mil/sti/trecms/pdf/AD1201034.pdf
-
     def __init__(self):
         self.GeometricLoss          = 0                             # Initialization of Variable
         self.ExtinctionLoss         = 0                             # Initialization of Variable
         self.TransmittedPower       = 0                             # Initialization of Variable
 
+    def geometric_loss(self, delft_telescope, led, orbit):
+        Dr = delft_telescope.LensDiameter * 10 ** (-3)                       # The diameter of the receiving aperture [m]
+        Dt = led.LED_emitter_diameter * 10 ** (-3)               # [m] The diameter of transmitting aperture (might not be applicable to LED) [m]
+        theta1 = led.divergence_angle * 10 ** (3)               # [mrad] Divergence Angle of the Beam [milli-radians]
+        L1 = orbit.OrbitAltitude                                # Link Distance [km]  COMMENT: FOR NOW IT IS AT JUST ORBIT HEIGHT, CAN BE CHANGED TO THE MAXIMUM LINK DISTANCE
+
+        Lgeom = -20 * np.log10(Dr / (Dt + theta1 * L1))  # dB
+        self.GeometricLoss = Lgeom
+
+    def extinction_loss(self, led):
+        # Extinction Loss Le #
+        lambda1 = led.wavelength        # Wavelength of the emitted light
+        delta1 = 1.6                # Particle Size Distribution Coefficient
+        V1 = 100 * 10 ^ 3           # [m] Meteorological Visibility (as this is relatred to the atmosphere it is also capped at 100km)
+        d1 = 100 * 10 ^ 3           # [m] Atmospheric Path Distance (This should be limited to 100km as maximum atmospheric thickness)
+
+        alphae                  = 3.91 / V1 * (lambda1 / 550) ** (-delta1)
+
+        Le                      = -10 * np.log10(np.e ** (-alphae * d1))  # dB
+        self.ExtinctionLoss     = Le
+
+
+    def link_budget(self, led):
+        La = 0
+        Gr = 0  # Assumed a receiver gain of 0 for safety
+        self.link_budget = led.P_transmitted - (Gr + La + self.ExtinctionLoss + self.GeometricLoss)
+
+
+class LinkBudget_source:
+
+    def __init__(self):
+        return
 
 class LinkBudget_TUD:                                               # Source: Slides from Dr. Speretta
-
     def __init__(self):
         self.GeometricLoss          = 0                             # Initialization of Variable
 
+
+
+led = LED()
+led.transmitted_power_link()
+constants = Constants()
+orbit = Orbit()
+delft_telescope = DelftTelescope()
+delft_telescope.maximum_link_distance(orbit, constants)
+
+linkBudget_naval = LinkBudget_Naval()
+linkBudget_naval.geometric_loss(delft_telescope, led, orbit)
+linkBudget_naval.extinction_loss(led)
+linkBudget_naval.link_budget(led)
+
+print("The Geometric Loss is: ", linkBudget_naval.GeometricLoss)
+print("The Extinction Loss is: ", linkBudget_naval.ExtinctionLoss)
+print("The Transmitted Power Link is: ", led.P_transmitted)
+print("The determined link budget of the LED is", linkBudget_naval.link_budget)
